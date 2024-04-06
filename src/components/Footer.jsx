a3a4bd910723ba98aa08d8005ac6da6b010ca18d
Nishant Giri <nishantgiri19722000@gmail.com>
2024-04-06 10:14:41 +0530

    ui changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,6 +11,13 @@ const Footer = () => {
           <em>@all rights reserved</em>
         </p>
       </div>
+      <div className="contents">
+        <ul>
+          <li>carrers</li>
+          <li>About</li>
+          <li>Home</li>
+        </ul>
+      </div>
     </footer>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,6 +14,7 @@ const Header = () => {
           <Link to={"/Services"}>Services</Link>
         </main>
       </nav>
+      <hr />
     </div>
   );
 };
